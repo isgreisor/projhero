7be082c8b942f5b2a1874088789d1c368f1ef998
isgreisor <62037937+isgreisor@users.noreply.github.com>
2020-08-14 11:03:54 -0400

    Final config files and updaes

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import MovieListView, MovieDetailView, \
                    MovieUpdateView, MovieDetailView, \
-                   MovieDeleteView, MovieCreateView
+                   MovieDeleteView, MovieCreateView, \
+                   SearchResultsListView
 
 urlpatterns = [
   path('<uuid:pk>/edit/', MovieUpdateView.as_view(), name='movie_edit'),
   path('<uuid:pk>/', MovieDetailView.as_view(), name='movie_detail'),
   path('<uuid:pk>/delete/', MovieDeleteView.as_view(), name='movie_delete'),
   path('new/', MovieCreateView.as_view(), name='movie_new'),
+  path('search/', SearchResultsListView.as_view(), name='search_results'),
   path('', MovieListView.as_view(), name='movie_list'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,32 +1,60 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.db.models import Q
 from .models import Movie
 # Create your views here.
 
-class MovieListView(ListView):
+class MovieListView(LoginRequiredMixin, ListView):
     model = Movie
     context_object_name = 'movie_list.html'
     template_name = 'movies/movie_list.html'
     fields = ("title","year","Dhero")
+    login_url = 'login'
 
-class MovieDetailView(DetailView):
+class MovieDetailView(LoginRequiredMixin, DetailView):
     model = Movie
     context_object_name = 'movie'
     template_name = 'movies/movie_detail.html'
+    login_url = 'login'
 
-class MovieUpdateView(UpdateView):
+class MovieUpdateView(LoginRequiredMixin, UpdateView):
     model = Movie
-    fields = ('title','year','Dhero')
+    fields = ('year','Dhero')
     template_name = 'movies/movie_edit.html'
+    login_url = 'login'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class MovieDeleteView(DeleteView):
+
+class MovieDeleteView(LoginRequiredMixin, DeleteView):
     model = Movie
     template_name = 'movies/movie_delete.html'
     success_url = reverse_lazy('movie_list')
+    login_url = 'login'
 
-class MovieCreateView(CreateView):
+
+class MovieCreateView(LoginRequiredMixin, CreateView):
     model = Movie
     template_name = 'movies/movie_new.html'
     fields = ('title', 'year', 'Dhero',)
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class SearchResultsListView(ListView):
+    model = Movie
+#    context_object_name = 'movie_detail'
+    template_name = 'movies/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Movie.objects.filter(
+          Q(title__icontains=query) | (Q(Dhero__icontains=query) |
+          Q(year__icontains=query))
+        )
